6746dadd43b9cda364633d54bafbd187c1aa67cb
sumeyaali <thesumeyaali@gmail.com>
2019-11-15 10:53:35 -0800

    unfinished sprint

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,45 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 export default function SearchForm() {
- 
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+
+useEffect(() => {
+  const results= characters.filter(character => 
+    character.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setSearchResults(results);
+  }, [searchTerm]);
+
+  const handleChange = e => {
+    setSearchTerm(e.target.value);
+  };
+
   return (
     <section className="search-form">
-     // Add a search form here
+     <form>
+        <lable htmlFor="name" >Search: </lable>
+        <input
+          id="name"
+          type="text"
+          name="textfield"
+          placeholder="Search"
+          onChange={handleChange}
+          value={searchTerm}
+        />
+     </form>
+     <div className="character-list">
+        <ul>
+          {searchResults.map(character => (
+            <li key={character}>{character}</li>
+          ))}
+        </ul>
+      </div>
+
     </section>
+
   );
+
 }
+
+
